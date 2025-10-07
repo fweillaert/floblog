e8c2f3fbdd02646b46a6ab6af166a896e523d6ed
Florent Weillaert <florent.weillaert@freeagent.com>
2025-10-07 15:18:51 +0100

    Add Jest testing setup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,12 +2,12 @@ import "dotenv";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-    schema: "./src/db/schema.ts",
-    out: "./src/db/migrations",
-    dialect: "postgresql",
-    dbCredentials: {
-        url: process.env.DATABASE_URL!,
-    },
-    verbose: true,
-    strict: true,
+  schema: "./src/db/schema.ts",
+  out: "./src/db/migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+  verbose: true,
+  strict: true,
 });
